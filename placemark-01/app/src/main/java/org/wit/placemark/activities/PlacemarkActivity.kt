865be17960f1fd1bdb81c06626070937975a1866
Judy OBrien <judyobrienie@gmail.com>
2018-09-23 10:58:57 +0100

    lab2-part3

--- placemark-01/app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ placemark-01/app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -7,32 +7,26 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.placemark.R
+import org.wit.placemark.main.MainApp
 import org.wit.placemark.models.PlacemarkModel
-
 
 class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
 
-
-  val placemarks = ArrayList<PlacemarkModel>()
   var placemark = PlacemarkModel()
+  lateinit var app : MainApp
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_placemark)
-    info("Placemark Activity started..")
+    app = application as MainApp
 
     btnAdd.setOnClickListener() {
-
-
       placemark.title = placemarkTitle.text.toString()
       placemark.description = placemarkDescription.text.toString()
-      println(placemarks)
-
-      if (placemark.title.isNotEmpty() && placemark.description.isNotEmpty()) {
-        placemarks.add(placemark.copy())
-        info("add Button Pressed: $placemark.Title")
-            placemarks.forEach { info("add Button Pressed: ${it.title}")}
-        info( "array: $placemarks")
+      if (placemark.title.isNotEmpty()) {
+        app.placemarks.add(placemark.copy())
+        info("add Button Pressed: $placemarkTitle")
+        app.placemarks.forEach { info("add Button Pressed: ${it}")}
       }
       else {
         toast ("Please Enter a title")
