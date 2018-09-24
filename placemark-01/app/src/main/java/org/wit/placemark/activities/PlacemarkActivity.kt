a00ca445d415756fdb6823cd770cf6ca2a1ec109
Judy OBrien <judyobrienie@gmail.com>
2018-09-24 12:28:31 +0100

    refactoring placemark adaptor

--- placemark-01/app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ placemark-01/app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -2,11 +2,15 @@ package org.wit.placemark.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_placemark.*
+import kotlinx.android.synthetic.main.activity_placemark_list.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.placemark.R
+
 import org.wit.placemark.main.MainApp
 import org.wit.placemark.models.PlacemarkModel
 
@@ -19,6 +23,7 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_placemark)
     app = application as MainApp
+
 
     btnAdd.setOnClickListener() {
       placemark.title = placemarkTitle.text.toString()
@@ -35,5 +40,20 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
         toast ("Please Enter a title")
       }
     }
+
   }
+
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.menu_placemarkactivity, menu)
+    return super.onCreateOptionsMenu(menu)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    when (item?.itemId) {
+      R.id.item_cancel -> finish()
+    }
+    return super.onOptionsItemSelected(item)
+  }
+
 }
